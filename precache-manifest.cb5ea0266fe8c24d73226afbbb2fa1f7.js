51290cec48316eb8cf54da739cf7c71ac7870a51
Sanagaram <vijayasai.sanagaram@siemens.com>
2020-07-21 18:22:17 +0530

    Updates

---
+++ precache-manifest.cb5ea0266fe8c24d73226afbbb2fa1f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89cbd83c546ec390500b852896084a1e",
+    "url": "/temp/index.html"
+  },
+  {
+    "revision": "9517a0c42a179350096f",
+    "url": "/temp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8362ea163fc1558bfbd3",
+    "url": "/temp/static/js/2.042f00b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/temp/static/js/2.042f00b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9517a0c42a179350096f",
+    "url": "/temp/static/js/main.08bd5e28.chunk.js"
+  },
+  {
+    "revision": "48618fd37d5a9f84c9ed",
+    "url": "/temp/static/js/runtime-main.fe661a04.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/temp/static/media/logo.5d5d9eef.svg"
+  }
+]);
